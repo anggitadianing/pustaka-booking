1e0f4f2d2f874c75e6986ac4f6ae585d1c2eb96e
anggitadianing <anggitadianing@gmail.com>
2021-10-05 22:09:47 +0700

    melakukan perubahan contoh1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,20 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contoh1 extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 * 
-	 * Maps to the following URL
-	 * 		https://example.com/index.php/welcome
-	 * - or -
-	 *		https://example.com/index/php/welcome/index
-	 * Since this controller is set as the default controller in
-	 * config/routed.php, it'is displayed at http://example.com/
-	 * 
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	
 	public function index()
 	{
