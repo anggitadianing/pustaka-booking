1f28105ac685f519cb7a729d7d352f8682a6e5e3
anggitadianing <anggitadianing@gmail.com>
2021-11-18 21:04:00 +0700

    pertemuan 9

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -16,7 +16,7 @@ class ModelUser extends CI_Model
     } 
  
     public function getUserWhere($where = null)     
-    {         
+    {        
         return $this->db->get_where('user', $where);     
     } 
  
@@ -33,44 +33,5 @@ class ModelUser extends CI_Model
         $this->db->from('user');         
         $this->db->limit(10, 0);         
         return $this->db->get();     
-    } 
- 
-=======
-<?php 
-defined('BASEPATH') or exit('No direct script access allowed'); 
- 
-class ModelUser extends CI_Model 
-{     
-
-    public function simpanData($data = null)
-    {         
-        $this->db->insert('user', $data);     
-    } 
- 
-    public function cekData($where = null)     
-    {         
-        return $this->db->get_where('user', $where);     
-    } 
- 
-    public function getUserWhere($where = null)     
-    {         
-        return $this->db->get_where('user', $where);     
-    } 
- 
-    public function cekUserAccess($where = null)     
-    {         
-        $this->db->select('*');         
-        $this->db->from('access_menu');         
-        $this->db->where($where);         
-        return $this->db->get();     
     }
-    public function getUserLimit()     
-    {         
-        $this->db->select('*');         
-        $this->db->from('user');         
-        $this->db->limit(10, 0);         
-        return $this->db->get();     
-    } 
- 
->>>>>>> 873421b4c06d73ff73299213cf49d12f4ac33831
-}  
+}
